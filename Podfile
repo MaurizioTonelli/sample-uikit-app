32d3ce59461ea620d0d84bab7009f3a30054932c
Maurizio Tonelli <mauriziotonelli@hotmail.com>
2023-10-27 14:25:56 -0600

    first commit

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '13.0'
+
+target 'sampleapp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+pod 'RealmSwift'
+pod 'IQKeyboardManagerSwift'
+pod 'SwipeCellKit'
+end
--- sampleapp/ViewController.swift
+++ sampleapp/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var addText: UITextField!
+    
+    let app = App(id: "application-sampleapp-reiug")
+    var realm = try! Realm()
+    
+    @Published var user: User?
+    @Published var configuration: Realm.Configuration?
+    @Published shops: Results<Shop>
+    
+    @objc func initialRealm() async {
+        user = try? await app?.login(credentials: Credentials.anonymous)
+        self.configuration = user?.flexibleSyncConfiguration(initialSubscriptions: { SyncSubscriptionSet in
+            if let _ = SyncSubscriptionSet.first(named: "all-shops") {
+                return
+            } else {
+                SyncSubscription.append(QuerySubscription<Shop>(name: "all-shops"))
+            }
+        }, rerunOnOpen: true)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func addAction(_ sender: Any) {
+    }
+    
 }
 
